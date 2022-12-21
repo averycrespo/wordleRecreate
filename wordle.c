63d684be8f0db76ad0d9428565fef853f8060ffe
averycrespo <120687390+averycrespo@users.noreply.github.com>
2022-12-20 21:47:32 -0500

    Added word check, modifying banner

--- wordle.c
+++ wordle.c
@@ -9,9 +9,79 @@
 #include <time.h>
 #include <ctype.h>
 #define MAX_LINE_NUM 5758
+#define NUMBER_OF_CHARS_WITH_NULL_TERMIN 6 
+
+void printBaner( void ) {
+    // printf("WW  WWWW  OOOOOOO  RRRRRRR  DDDDD    LLL  EEEEEEE\n");
+    // printf("WWW       OO   OO  RRRRRRR  DD   DD  LLL  EEEEEEE\n");
+    printf("WWW       WWW         WWW\n");
+    printf("WWWWW      WWW      WWWWW\n");
+    printf("WWWWWW     WWW     WWWWWW\n");
+    printf("WWWWWWW    WWW    WWWWWWW\n");
+    printf("WWWWWWWW   WWW   WWWWWW\n");
+    printf("WWWWWWWWWW   WWWW   WWWW\n");
+
+}
+
+/**
+ * Dynamically allocate a list of avaliable words from input.txt
+ * @return 2D array of entire list of strings from input.txt
+*/
+char** createList( void ) {
+    FILE *fp = fopen("input.txt", "r");
+
+    //Word to be guessed 
+    char string[6];
+
+    //Allocate for list
+    char **list = malloc( MAX_LINE_NUM  * sizeof( char * ) );
+    //allocate each string 
+    for(int i = 0; i < MAX_LINE_NUM; i++) {
+        fscanf( fp, "%s", string );
+        // list[i] = malloc( )
+        list[i] = malloc(( NUMBER_OF_CHARS_WITH_NULL_TERMIN ) * sizeof(char));
+        strcpy( list[i], string);
+    }
+    //Close file 
+    fclose( fp );
+    //return list 
+    return list;
+}
+
+/**
+ * Frees allocated list 
+ * @param list entire list of valid 5 letter words from input.txt
+*/
+void freeList( char **list ) {
+    //Loop through all allocated strings
+    for(int i = 0; i < MAX_LINE_NUM; i++) {
+      free( list[i] );
+    }
+    //Free list 
+    free(list);
+}
+
+/**
+ * Checks if word provided from userInput is a word from the existing list
+ * @param list a list of valid words from the input file
+ * @param userInput is the character array of what the user entered
+ * @return true if word is valid
+ * @return false if word is invalid
+*/
+bool isValidWord( char **list, char userInput[] ) {
+    //Loop through all words 
+    //Could be optimized if word list was sorted 
+    for( int i = 0; i < MAX_LINE_NUM; i++) {
+        if( strcmp(list[i], userInput) == 0) {
+            return true;
+        }
+    }
+    return false; 
+}
 
 /**
  * Get a 'random' word from a list of 5 letter words 
+ * @return i the integer generated from the list of 5758 number of words
 */
 int generateNum( void ) {
     //Generate a number to scan through text file and stop 
@@ -39,7 +109,7 @@ bool validatePlacements( char arr[], char string[] ) {
             flag = false;
             //Boolean to check if any correct letter was found in the incorrect placement 
             bool printRegular = true;
-            for(int j = 0; j < 5; j++) {
+            for(int j = 0; j < 4; j++) {
                 if( arr[i] == string[j]) { 
                     printf("\033[0;33m"); //Set the text to the color green
                     printf("%c", arr[i]);
@@ -93,6 +163,13 @@ void saveStats( int counter ) {
  * Main function for wordle 
 */
 int main(int argc, char *argv[] ) {
+
+    //Printing banner
+    printBaner();
+
+    //Create list
+    char **list = createList();
+
     //Start counter 
     int counter = 0;
 
@@ -132,12 +209,16 @@ int main(int argc, char *argv[] ) {
             userGuess[i] = tolower(ch); 
             i++; 
         } 
-        if( strcmp(userGuess, "quit") == 0 ) {  //If user calls to quit 
+        if( strcmp( userGuess, "quit" ) == 0 ) {  //If user calls to quit 
             printf("The word was: %s\n", string);
             return EXIT_SUCCESS;
-        } else if( strlen( userGuess ) != 5 ) {
+        } else if( strlen( userGuess ) != 5 ) { //User entered word that is not a five lettered word 
             printf("Invalid guess\n");
-        }  else {
+            counter--;
+        } else if( isValidWord( list, userGuess) == false ) { //User entered word not in list 
+            printf("Not in word list\n");
+            counter--;
+        } else {
             flag = validatePlacements( userGuess, string );
             if( flag ) {
                //Increment counter to adjust for rounds
@@ -147,16 +228,19 @@ int main(int argc, char *argv[] ) {
                 //Save stats 
                 saveStats( counter ); 
                 printf("Stats saved to --> stats.txt\n");
+                //Free list 
+                freeList( list );
                 return EXIT_SUCCESS;
             }
         }
         counter++;
-
-        printf("The word was: %s\n", string);
     } //end of loop
 
     //If reached
-    printf("You lost, the word was: %s\n", string);
+    printf("You lost, the word was: %s\n", string); 
+
+    //Free list 
+    freeList( list );
     
     //exit correctly
     return EXIT_SUCCESS;
